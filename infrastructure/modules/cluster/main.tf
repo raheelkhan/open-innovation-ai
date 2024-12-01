5aff9b5a8a5ae6a0de2176b34628d20d224f1d05
Admin <admin@Admins-MacBook-Pro.local>
2024-12-01 15:44:18 +0400

    Add helm chart to mimic the frontend component of our application. It contains some main resources such as Deployment, Ingress with proper alb class so that AWS Cloud Controller will spin an Application Load Balancer for this ingress, Also there is HPA configured with some dummy resource utilization tracking. There is a Service created that is called by the Ingress. For security there are some securityContext related configs as well

--- infrastructure/modules/cluster/main.tf
+++ infrastructure/modules/cluster/main.tf
@@ -5,9 +5,9 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access       = true
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
-  cluster_endpoint_private_access = true
+  cluster_endpoint_private_access      = true
 
   cluster_addons = {
     coredns                = {}
@@ -16,9 +16,9 @@ module "eks" {
     vpc-cni                = {}
   }
 
-  vpc_id                   = var.vpc_id
-  subnet_ids               = var.subnet_ids
-  
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
   eks_managed_node_group_defaults = {
     min_size     = var.min_size
     max_size     = var.max_size
@@ -29,15 +29,21 @@ module "eks" {
     cpu = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.medium"]
+      labels = {
+        "open.innovation.ai/node-type" = "cpu"
+      }
     }
     gpu = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["p3.2xlarge"]
+      labels = {
+        "open.innovation.ai/node-type" = "gpu"
+      }
     }
   }
 
   enable_cluster_creator_admin_permissions = true
-  
+
   tags = {
     Environment = var.environment
   }
--- infrastructure/modules/cluster/variables.tf
+++ infrastructure/modules/cluster/variables.tf
@@ -15,26 +15,26 @@ variable "subnet_ids" {
 }
 
 variable "environment" {
-  type = string
+  type        = string
   description = "Environment such as production, staging etc"
 }
 
 variable "min_size" {
-  type = number
+  type        = number
   description = "Minimum number of worker nodes in a node group"
 }
 
 variable "max_size" {
-  type = number
+  type        = number
   description = "Minimum number of worker nodes in a node group"
 }
 
 variable "desired_size" {
-  type = number
+  type        = number
   description = "Desired number of worker nodes in a node group"
 }
 
 variable "cluster_endpoint_public_access_cidrs" {
-  type = list(string)
+  type        = list(string)
   description = "CIDR to allow access to cluster endpoint"
 }
